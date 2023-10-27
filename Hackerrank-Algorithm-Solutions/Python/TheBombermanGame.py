037eb4ab7c562d547065c1e719ab1f1cb9333149
e-hokage777 <e.hokage777@gmail.com>
2023-10-27 11:15:44 +0000

    updated bomberman

--- Hackerrank-Algorithm-Solutions/Python/TheBombermanGame.py
+++ Hackerrank-Algorithm-Solutions/Python/TheBombermanGame.py
@@ -1,25 +1,26 @@
 import sys
 
-states = {"init": 0, "full": 1, "alt": 2}
+states = {"alt2": 0, "full": 1, "alt": 2}
 
-def bomberman(n, grid):
+def bomberMan(n, grid):
     current_state = states["init"]
     
     if n > 1:
         if n%2 == 0:
             current_state = states["full"]
-        elif in_alt_series:
-            current_state = states["alt"]
+        elif in_alt_series(n):
+            current_state = states["alt2"]
 
     return create_ans_grid(grid, current_state)
 
 def create_ans_grid(grid, state):
     match state:
         case 0:
-            return grid
+            return create_alt_grid(create_alt_grid(grid))
         case 1:
             return create_full_grid(grid)
         case 2:
+            print("here")
             return create_alt_grid(grid)
         
 
@@ -31,13 +32,13 @@ def create_full_grid(grid):
 
 def create_alt_grid(grid):
     full_grid = create_full_grid(grid)
+    full_grid = list(map(list, full_grid))
     nr = len(grid)
     nc = len(grid[0])
 
     for r in range(nr):
         for c in range(nc):
             if grid[r][c] == "O":
-                print("found")
                 full_grid[r][c] = "."
                 left = 0 if c-1 < 0 else c-1
                 right = nc-1 if c+1 > nc-1 else c+1
@@ -49,13 +50,13 @@ def create_alt_grid(grid):
                 full_grid[up][c] = "."
                 full_grid[down][c] = "."
     
-    return full_grid
+    return list(map(lambda x: "".join(x), full_grid))
 
 def in_alt_series(n):
-    return ((n-5)/4)%4 == 0
+    return ((n-3)/4)%4 == 0
 
 
 if __name__=="__main__":
     lines = sys.stdin.readlines()
     lines = list(map(lambda x: x.strip(), lines))
-    print(bomberman(5,lines))
+    print("\n".join(bomberMan(5,lines)))
